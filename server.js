846c5d8120f3a89cff69624611de13951b026c17
McDonn117 <mcdonn.dagum@gmail.com>
2019-11-22 18:51:41 +0800

    Initial Commit

--- server.js
+++ server.js
@@ -1,32 +1,49 @@
-// server.js
-// where your node app starts
+//jshint esversion:6
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
 });
 
+app.get("/api/timestamp/", (req, res) => {
+  let newDate = new Date();
+  res.json({ unix: newDate.getTime(), utc: newDate.toUTCString() });
+});
 
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+
+  if (/\d{5,}/.test(dateString)) {
+    let dateInt = parseInt(dateString);
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  }
+
+  let newDate = new Date(dateString);
+
+  if (newDate.toUTCString() !== "Invalid Date") {
+    res.json({ unix: newDate.valueOf(), utc: newDate.toUTCString() });
+  } else {
+    res.json({ error: "Invalid Date" });  }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
